547f12364255904418ab956edad401171a921537
IUT GEII Toulon Table 5 <houssemmatmati2@gmail.com>
2021-12-03 15:23:09 +0100

    envoyer-recevoir

--- C_Sharp/RobotInterface/MainWindow.xaml.cs
+++ C_Sharp/RobotInterface/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExtendedSerialPort;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace RobotInterface
 {
     /// <summary>
@@ -20,16 +23,47 @@ namespace RobotInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        ReliableSerialPort serialPort1;
         public MainWindow()
         {
             InitializeComponent();
+            
+
+            serialPort1 = new ReliableSerialPort("COM15", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.Open();
         }
 
+        Boolean isColor;
         private void ButtonEnvoyer_Click(object sender, RoutedEventArgs e)
         {
+            if (isColor == true)
+            {
+                ButtonEnvoyer.Background = Brushes.RoyalBlue;
+                isColor = false;
+            }
+            if (isColor == false)
+            {
+                ButtonEnvoyer.Background = Brushes.Beige;
+                isColor = true;
+            }
+            
+            SendMessage();
+        }
 
+        private void Grid_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter )
+            {
+                SendMessage();
+            }
+            
+        }
 
-            ButtonEnvoyer.Background = Brushes.RoyalBlue;
+        void SendMessage()
+        {
+            //RichTexBox.Text += "Reçu : " + textBoxEmission.Text;
+            serialPort1.WriteLine(textBoxEmission.Text);
+            textBoxEmission.Text = "";
             
         }
     }
